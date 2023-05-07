0db1856de2f7d0732378a8445002fbbf890c1d0b
Fitsum Mekonnen <official.fitsum.mekonnen@gmail.com>
2023-05-07 03:40:10 +0300

    Finished and Added the Stat Cards

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,29 +1,30 @@
-import { getClient } from "@/apollo-client"
+import { getClient } from "@/apollo-client";
 import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 
 type Props = {
-    params: {
-        city: string;
-        lat: string;
-        long: string;
-    }
-}
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
+};
 
-async function WeatherPage({params: {city, lat, long}}: Props) {
-    const client = getClient();
-    const { data, loading, error } = await client.query({
-        query: fetchWeatherQuery,
-        variables: {
-            current_weather: "true",
-            latitude: lat,
-            longitude: long,
-            timezone: "GMT"
-        }
-    });
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+  const { data, loading, error } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      latitude: lat,
+      longitude: long,
+      timezone: "GMT",
+    },
+  });
 
-    const results: Root = data.myQuery;
-    console.log(results);
+  const results: Root = data.myQuery;
+  console.log(results);
   return (
     <div>
       {/* <InformationPanel/> */}
@@ -38,13 +39,56 @@ async function WeatherPage({params: {city, lat, long}}: Props) {
             </p>
           </div>
 
-          <div>
+          <div className="m-2 mb-10">
             <CalloutCard message="Hi, I’ll be here until ChatGPT comes." />
           </div>
+
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+            <StatCard
+              title="Maximum Temperature"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+              color="yellow"
+            />
+
+            <StatCard
+              title="Minimum Temperature"
+              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+              color="green"
+            />
+
+            <div>
+              <StatCard
+                title="UV Index"
+                metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
+                color="rose"
+              />
+              {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                <CalloutCard
+                  message={"The UV is high today, be sure to wear SPF!"}
+                  warning
+                />
+              )}
+            </div>
+
+            <div className="flex space-x-3">
+              <StatCard
+                title="Wind Speed"
+                metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                color="cyan"
+              />
+
+              <StatCard
+                title="Wind Direction"
+                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                color="violet"
+              />
+            </div>
+          </div>
         </div>
+        <hr className="mb-5" />
       </div>
     </div>
   );
 }
 
-export default WeatherPage
+export default WeatherPage;
